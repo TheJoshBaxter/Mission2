e5d9b061bcb424fb3b8c0e47a76622c8932031a0
Joshua Baxter <jebaxter2@gmail.com>
2022-01-10 16:56:27 -0700

    First Commit

---
+++ wwwroot/js/gradeCalc.js
@@ -0,0 +1,12 @@
+﻿function calcGrade() {
+    fAssignment = $('#assignments').val()
+    fGroupProjects = $('#groupProjects').val()
+    fQuizzes = $('#quizzes').val()
+    fExams = $('#exams').val()
+    fIntex = $('#intex').val()
+
+    fFinalGrade = (fAssignment * .55) + (fGroupProjects * .05) + (fQuizzes * .1) + (fExams * .2) + (fIntex * .1)
+    fFinalGrade = fFinalGrade * 100
+    fFinalGrade = Math.round(fFinalGrade)
+    alert("Your final grade was " + fFinalGrade + "%")
+}
