22396dc26284744b8950bf821d5c4eb7c8a9c336
Joshua Baxter <jebaxter2@gmail.com>
2022-01-10 17:07:54 -0700

    added comments

--- wwwroot/js/gradeCalc.js
+++ wwwroot/js/gradeCalc.js
@@ -1,4 +1,5 @@
 ﻿function calcGrade() {
+    //stores the form inputs to js variables
     fAssignment = $('#assignments').val()
     fGroupProjects = $('#groupProjects').val()
     fQuizzes = $('#quizzes').val()
@@ -6,7 +7,7 @@
     fIntex = $('#intex').val()
 
     fFinalGrade = (fAssignment * .55) + (fGroupProjects * .05) + (fQuizzes * .1) + (fExams * .2) + (fIntex * .1)
-    fFinalGrade = fFinalGrade * 100
+    fFinalGrade = fFinalGrade * 100 //make decimal into percentage
     fFinalGrade = Math.round(fFinalGrade)
-    alert("Your final grade was " + fFinalGrade + "%")
+    alert("Your final grade was " + fFinalGrade + "%") //output
 }
