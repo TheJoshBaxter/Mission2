e37f19d271fc001a5649619971a58985e26b454f
Joshua Baxter <jebaxter2@gmail.com>
2022-01-17 15:43:22 -0700

    mission 3 commit
    first commit

---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0,100)]
+        public int Assignments { get; set; }
+
+        [Range(0, 100)]
+        public int GroupProjects { get; set; }
+
+        [Range(0, 100)]
+        public int Quizzes { get; set; }
+
+        [Range(0, 100)]
+        public int Exams { get; set; }
+
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
